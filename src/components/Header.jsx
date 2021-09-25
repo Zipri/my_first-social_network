dd3be23c6a396b74eb12fc91aabbe54a607e15bc
Zipri <42815802+Zipri@users.noreply.github.com>
2021-09-25 14:52:11 +0300

    14 lesson (css-modules)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import s from './Header.module.css'
 
 const Header = () => {
 	return (
-		<header className='header'>
+		<header className={s.header}>
 			<img src='https://upload.wikimedia.org/wikipedia/ru/c/cf/Лобос_УПНФМ_%28лого%29.png'/>
 		</header>
 	)
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import s from './Profile.module.css'
 
 const Profile = () => {
 	return (
-		<div className='content'>
+		<div className={s.content}>
 			<div>
 				<img src='https://gazetaingush.ru/sites/default/files/pubs/obshchestvo/2021/05/priroda-ingushetii-1200x5401.jpg'/>
 			</div>
@@ -17,10 +18,10 @@ const Profile = () => {
 				</div>
 			</div>
 			<div>
-				<div>
+				<div className={s.item}>
 					post 1
 				</div>
-				<div>
+				<div className={s.item}>
 					post 2
 				</div>
 			</div>
