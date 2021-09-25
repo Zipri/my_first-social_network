eee781124134e066ee3f85b1258da52aa858ac99
Zipri <42815802+Zipri@users.noreply.github.com>
2021-09-25 13:18:20 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+	return (
+		<header className='header'>
+			<img src='https://upload.wikimedia.org/wikipedia/ru/c/cf/Лобос_УПНФМ_%28лого%29.png'/>
+		</header>
+	)
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Profile = () => {
+	return (
+		<div className='content'>
+			<div>
+				<img src='https://gazetaingush.ru/sites/default/files/pubs/obshchestvo/2021/05/priroda-ingushetii-1200x5401.jpg'/>
+			</div>
+			<div>
+				<img src='https://lh3.googleusercontent.com/proxy/aPah90hgPI4g6fBEiFVQyq_2t89TVpc1LGuF2n5Utw4_wIK6VOPbL28NBB60Bc9eNOTQ4svUBxmV3JtXEvrMwVRdhVaUcn2gPsPjMqtCNNBAdNpwAeg5rQ1wsh83phim7g0EzwdOfXM'/>
+				ava + description
+			</div>
+			<div>
+				my posts
+				<div>
+					new posts
+				</div>
+			</div>
+			<div>
+				<div>
+					post 1
+				</div>
+				<div>
+					post 2
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Profile;
