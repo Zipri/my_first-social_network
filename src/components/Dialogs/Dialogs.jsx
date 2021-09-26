5ab8043da3966b078cc090d42d13f1caf0faaa32
Zipri <42815802+Zipri@users.noreply.github.com>
2021-09-26 18:28:28 +0300

    20/21 lesson (added NavLinks & grid to Dialogs)

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,36 @@ import s from './Dialogs.module.css'
 
 const Dialogs = () => {
 	return (
-		<div>
-			Dialogs
+		<div className={s.dialogs}>
+
+			<div className={s.dialogsItems}>
+				<div className={s.dialog}>
+					Собака
+				</div>
+				<div className={s.dialog}>
+					Валера
+				</div>
+				<div className={s.dialog}>
+					Кот
+				</div>
+				<div className={s.dialog}>
+					Черьв
+				</div>
+				<div className={s.dialog}>
+					Кто-то
+				</div>
+				<div className={s.dialog}>
+					Шнур
+				</div>
+			</div>
+
+			<div className={s.messages}>
+				<div className={s.message}>Первое сообщение</div>
+				<div className={s.message}>Второе сообщение</div>
+				<div className={s.message}>Powel nahoi</div>
+				<div className={s.message}>प्रतीक 'मार्ग'निराकरण करू शकत नाही</div>
+			</div>
+
 		</div>
 	)
 }
