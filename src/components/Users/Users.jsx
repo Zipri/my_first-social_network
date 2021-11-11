b28ead63dbba54ae424b2b7dec49bf693387c36f
Zipri <42815802+Zipri@users.noreply.github.com>
2021-11-11 11:36:45 +0300

    54 lesson (componentDidMount() at Users)

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -43,8 +43,8 @@ let UserLabel = (props) => {
 }
 
 class Users extends React.Component {
-	constructor(props) {
-		super(props);
+
+	componentDidMount() {
 		axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
 			this.props.setUsers(response.data.items)
 		})
